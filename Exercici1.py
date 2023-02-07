8ef9f8eac7ef362bdf98f989c6286ebdd55075e4
ksama22 <ksama22@jaumebalmes.net>
2023-02-07 20:42:29 +0100

    Ex1 y 2

--- Exercici1.py
+++ Exercici1.py
@@ -1,4 +1,39 @@
 import matplotlib.pyplot as plt
+import pandas as pd
 import numpy as np
+
+
 def hi():
     print("Hi")
+
+def Ex1NotesMitjanes(arxiu):
+    # Dropna quita los null
+    df = pd.read_csv(arxiu, usecols=["NAME", "NOTES"]).dropna()
+    # https://pandas.pydata.org/pandas-docs/stable/getting_started/comparison/comparison_with_r.html?highlight=avg
+    # Agrupa por notas segun el nombre y hace la media
+    df = df.groupby('NAME').agg({'NOTES': 'mean'})
+    print(df)
+    #Esto crea una grafica con esta array pero no de x e y como deberia
+    plt.plot([1, 2, 3, 4, 5], [1, 2, 3, 4, 5], 'g-')
+    plt.title("MITJA NOTA ALUMNAT CICLE")
+    plt.ylabel('ALUMNAT')
+    plt.xlabel('NOTES')
+    #Muestra el grafico
+    plt.show()
+
+
+def Ex2Faltes(arxiu):
+    # Dropna quita los null
+    df = pd.read_csv(arxiu, usecols=["NAME", "ABSENCES"]).dropna()
+    # https://pandas.pydata.org/pandas-docs/stable/getting_started/comparison/comparison_with_r.html?highlight=avg
+    # Agrupa por notas segun el nombre y hace la SUMA
+    df = df.groupby('NAME').sum({'ABSENCES': 'mean'})
+    print(df)
+    #Esto crea una grafica con esta array pero no de x e y como deberia
+    plt.plot([1, 2, 3, 4, 5], [1, 2, 3, 4, 5], 'g-')
+    plt.title("% de faltes DAW2")
+    plt.ylabel('FALTES EN %')
+    plt.xlabel('Alumnat')
+    #plt.legend()
+    #Muestra el grafico
+    plt.show()
--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 import Exercici1 as ex1
 
+arxiu = "Llistat.csv"
 
-ex1.hi()
+#Mostra les notes mitjanes
+ex1.Ex1NotesMitjanes(arxiu)
+
+#Mostra les faltes
+ex1.Ex2Faltes(arxiu)
